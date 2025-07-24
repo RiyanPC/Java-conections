c0726075ef1715383c328098d2679e4fe74b612f
RiyanPC <locochon56303@example.com>
2025-07-24 11:10:23 -0500

    conection

--- packages/models/conexion.java
+++ packages/models/conexion.java
@@ -1,6 +1,22 @@
 package packages.models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class conexion {
-    
+
+    Connection con;
+
+    public Connection getConnection() {
+        try {
+            String myBD = "jdbc:mysql://localhost:3306/db_prueba?serverTimezone=UTC";
+            con = DriverManager.getConnection(myBD, "root", "");
+            return con;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return null;
+    }
     
 }
